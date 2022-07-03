83f4ce63cd451011350af8aa72bedf1a10eeef77
Grant Luna <grant.luna@fnf.com>
2022-07-02 19:50:24 -0700

    Create result_hash with default value of 0

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,6 +3,8 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(string, array)
   # Create an array from the parameter string
   string_array = string.downcase.split
+  # Create a hash with default values of 0
+  result_hash = Hash.new(0)
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
