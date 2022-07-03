fdcc8661964c0aad55ddbf505a77cc04d7bb4014
Grant Luna <grant.luna@fnf.com>
2022-07-02 19:53:52 -0700

    Create two loops.  String_array loop for each element and an interior loop on the array parameter.  Check if each element from string_array includes an element from the parameter array.  If it does, add 1 to the result_hash value for the key of the given parameter array element.

--- sub_strings.rb
+++ sub_strings.rb
@@ -5,6 +5,15 @@ def substrings(string, array)
   string_array = string.downcase.split
   # Create a hash with default values of 0
   result_hash = Hash.new(0)
+  # Loop through each element in string array
+  string_array.each do |word|
+    # loop through each element in the parameter array
+    array.each do |dictionary_word|
+      if word.include?(dictionary_word)
+        result_hash[dictionary_word] += 1
+      end
+    end
+  end
 end
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
